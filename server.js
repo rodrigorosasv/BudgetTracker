7a801b57508a2f4c3587ee8098e6dde400d7a934
Rodrigo Rosas <rodrigo.rosasv@gmail.com>
2020-08-11 18:20:00 -0500

    Heroku mongo adjustment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-var MONGODB_URI=process.env.MONGODB_URI || "mongodb://localhost/budget";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 // mongoose.connect("mongodb://localhost/budget", {
 //   useNewUrlParser: true,
